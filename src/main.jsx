153f0290160003e160ff5a30516a6093546e9b9c
Umar Farooq <umaarzaman21@gmail.com>
2026-03-20 02:18:42 -0700

    Fix base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/shoes-web">
+    <BrowserRouter basename="/sneaker_web">
       <StoreProvider>
         <App />
       </StoreProvider>
